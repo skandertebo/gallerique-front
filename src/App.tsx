678ce27423e667d9e07762eda5823951c284f49e
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-04-23 13:38:40 +0100

    setup apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
 import "./App.css";
-
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
 }
