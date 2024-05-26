b8c2fa3bcac59fe94761aa382d14946211890569
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-05-26 17:41:46 +0100

    fix: updated auction card to have remaining time

---
+++ src/utils/formatRemainingTime.ts
@@ -0,0 +1,16 @@
+export default function formatRemainingTime(endTime: string): string {
+  const remainingTime = new Date(endTime).getTime() - new Date().getTime();
+  if (remainingTime < 0) {
+    return "00:00:00";
+  }
+  const hours = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  return (
+    `${hours < 10 ? "0" + hours : hours}:` +
+    `${minutes < 10 ? "0" + minutes : minutes}:` +
+    `${seconds < 10 ? "0" + seconds : seconds}`
+  );
+}
